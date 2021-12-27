90f30428b0b59571b24aa8c0ec12b07a81fcf875
rmyau <91686883+rmyau@users.noreply.github.com>
2021-12-27 23:39:47 +0300

    Дописано второе задание

--- DZ_2/Program.cs
+++ DZ_2/Program.cs
@@ -51,26 +51,56 @@ namespace DZ_2
             s[index] = alf[ind_in_alf + 1];
 
         }
+        public static void NextRearrangement(List<string> ReArr)//perestnovka
+        {
+            int index = ReArr.Count - 2;
+            while (alf.IndexOf(ReArr[index]) > alf.IndexOf(ReArr[index + 1]))
+                index--;
+            int s = index + 1;
+            while (s < ReArr.Count - 1 && (alf.IndexOf(ReArr[s + 1]) > alf.IndexOf(ReArr[index])))
+                s++;
+            string t=ReArr[index];//поменяли местами полученный и наименьший справа
+            ReArr[index] = ReArr[s];
+            ReArr[s] = t;
+            
+            for (int i = 0; i < (ReArr.Count - index - 1) / 2; i++)
+            {
+                t = ReArr[ReArr.Count - 1 - i];
+                ReArr[ReArr.Count - 1 - i] = ReArr[index + i + 1];
+                ReArr[index + i + 1]=t;
+            }
 
-        //static bool hasNextArrange(List<string> arr)
-        //{
-        //    if (arr[m-k-1]==alf[m-1])
+        }
+        public static bool hasNextRearrangement(List<string> ReArr)
+        {
+            int index = ReArr.Count - 1;
+            while (index > 0 && (alf.IndexOf(ReArr[index]) < alf.IndexOf(ReArr[index - 1])))
+                index--;
+            if (index == 0) return false;
+            else return true;
+        }
 
-        //        for (int i=m-k-1;i>=0;i--)
-        //            if ()
-        //}
-        public static void Connect(List<int> sochet, List<string> arrange)
+        public static void NextArrange(List<string> arr, List<string> ReArr)
+        {
+            NextRearrangement(ReArr);
+            for (int i = 0; i < m - k; i++)
+                arr[i] = ReArr[i];
+            if (hasNextRearrangement(ReArr))
+                NextRearrangement(ReArr); 
+        }
+        
+        public static void Connect(List<int> sochet, List<string> arrange,List<string> word)
         {
             int indexAr = 0;
             for (int i = 0; i < m; i++)
-                word1[i] = "";
+                word[i] = "";
 
             for (int i = 0; i < k; i++)
-                word1[sochet[i]] = alf[0];
+                word[sochet[i]] = alf[0];
             for (int i = 0; i < m; i++)
-                if (word1[i] == "")
+                if (word[i] == "")
                 {
-                    word1[i] = arrange[indexAr];
+                    word[i] = arrange[indexAr];
                     indexAr++;
                 }
         }
@@ -100,6 +130,7 @@ namespace DZ_2
 
             List<string> arrange1 = new List<string>();
             List<string> arrange2 = new List<string>();
+            List<string> perest = new List<string>();
             for (int i = 0; i < m - k; i++)
             {
                 arrange1.Add("");
@@ -114,26 +145,38 @@ namespace DZ_2
             {
                 word1.Add("");
                 word2.Add("");
+                perest.Add(alf[i+1]);
             }
-
+            file1.WriteLine("Все слова длины 5, в которых две буквы а:");
+            file2.WriteLine("Все слова длины 5, в которых две буквы а, остальные буквы не повторяются:");
             while (hasNextSochet(sochet))
             {
                 NextSochet(sochet);
                 for (int i = 0; i < m - k; i++)
                     arrange1[i] = "b";
 
-                Connect(sochet, arrange1);
+                Connect(sochet, arrange1, word1);
                 Print(word1, file1);
                 while (hasNextArrangeRepeat(arrange1))
                 {
                     NextArrangeRepeat(arrange1);
-                    Connect(sochet, arrange1);
+                    Connect(sochet, arrange1, word1);
                     Print(word1, file1);
                 }
 
-
+                for (int i = 0; i < m; i++)
+                    perest[i] = alf[i + 1];
+                NextArrange(arrange2, perest);
+                Connect(sochet, arrange2, word2);
+                Print(word2, file2);
+                while (hasNextRearrangement(perest))
+                {
+                    NextArrange(arrange2, perest);
+                    Connect(sochet, arrange2, word2);
+                    Print(word2, file2);
+                }
             }
-
+                
             file1.Close();
             file2.Close();
             //Console.ReadKey();
